afcaa1fc0e0086e4968e444e653962bb96df9b61
Michael Wieland <git@selfcoders.com>
2018-05-11 02:58:31 +0200

    Use per day log files, support for multiple templates

--- convert.php
+++ convert.php
@@ -6,16 +6,62 @@ use Symfony\Component\Finder\SplFileInfo;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
+function getMessages(SplFileInfo $file)
+{
+    // Trillian does not include a root element in the XML files
+    $xml = "<root>" . $file->getContents() . "</root>";
+
+    $dom = new DOMDocument;
+
+    $dom->loadXML($xml);
+
+    return $dom->getElementsByTagName("message");
+}
+
+function timestampToDate($timestamp)
+{
+    $date = new DateTime;
+
+    $date->setTimestamp($timestamp / 1000);// Timestamp is in milliseconds
+
+    return $date;
+}
+
+function getMessageFromElement(DOMElement $element)
+{
+    //<message time="1311612005103" type="outgoing_privateMessage" text="some%20message"/>
+    $date = timestampToDate($element->getAttribute("time"));
+    $type = $element->getAttribute("type");
+    $text = rawurldecode($element->getAttribute("text"));
+
+    $messageId = sha1(sprintf("%d/%s/%s", $date->getTimestamp(), $type, $text));
+
+    return array
+    (
+        $date,
+        $type,
+        $text,
+        $messageId
+    );
+}
+
+function getFilenameFromDate(DateTime $date)
+{
+    //2011-07-25.184005+0200CEST
+    return $date->format("Y-m-d.HisOT") . ".html";
+}
+
 if (!isset($argv[3])) {
-    fwrite(STDERR, "Usage: " . $argv[0] . " <own username> <path to Trillian logs dir> <output dir>\n");
+    fwrite(STDERR, "Usage: " . $argv[0] . " <own username> <path to Trillian logs dir> <output dir> [<template>]\n");
     exit(1);
 }
 
 $ownName = $argv[1];
 $sourceDir = $argv[2];
 $outputDir = $argv[3];
+$template = $argv[4] ?? "bootstrap";
 
-$loader = new Twig_Loader_Filesystem(__DIR__);
+$loader = new Twig_Loader_Filesystem(__DIR__ . "/templates");
 $twig = new Twig_Environment($loader);
 
 $filesystem = new Filesystem;
@@ -45,35 +91,43 @@ foreach ($finder as $file) {
 }
 
 foreach ($perContactFiles as $contactName => $files) {
+    $perDayMessages = array();
+
+    foreach ($files as $file) {
+        foreach (getMessages($file) as $messageElement) {
+            /**
+             * @var $date DateTime
+             */
+            list($date, $type, $text, $messageId) = getMessageFromElement($messageElement);
+
+            $day = $date->format("Y-m-d");
+
+            if (!isset($perDayMessages[$day])) {
+                $perDayMessages[$day] = array();
+            }
+
+            if (isset($perDayMessages[$day][$messageId])) {
+                fwrite(STDERR, sprintf("Skipping duplicate message: %s - %s\n", $date->format("r"), $text));
+                continue;
+            }
+
+            $perDayMessages[$day][$messageId] = array($date, $type, $text);
+        }
+    }
+
+    // Remove messageId key
+    $perDayMessages = array_map("array_values", $perDayMessages);
+
+    $perDayMessages = array_values($perDayMessages);
+
     $previousLog = null;
 
-    /**
-     * @var $file SplFileInfo
-     */
-    foreach ($files as $fileIndex => $file) {
-        // Trillian does not include a root element in the XML files
-        $xml = "<root>" . $file->getContents() . "</root>";
-
-        $dom = new DOMDocument;
-
-        $dom->loadXML($xml);
-
+    foreach ($perDayMessages as $dayIndex => $dayMessages) {
         $messages = array();
+        $firstDate = null;
 
-        //<message time="1311612005103" type="outgoing_privateMessage" text="some%20message"/>
-        /**
-         * @var $message DOMElement
-         */
-        foreach ($dom->getElementsByTagName("message") as $message) {
-            $timestamp = $message->getAttribute("time") / 1000;// Timestamp is in milliseconds
-            $type = $message->getAttribute("type");
-            $text = rawurldecode($message->getAttribute("text"));
-
-            $messageId = sha1(sprintf("%d/%s/%s", $timestamp, $type, $text));
-
-            $date = new DateTime;
-
-            $date->setTimestamp($timestamp);
+        foreach ($dayMessages as $message) {
+            list($date, $type, $text) = $message;
 
             switch ($type) {
                 case "incoming_privateMessage":
@@ -92,12 +146,11 @@ foreach ($perContactFiles as $contactName => $files) {
             // Inline images
             $text = preg_replace("/(http|https):\/\/(ft|media).trillian.im\/([^ ]+)/", '<a href="${1}://${2}.trillian.im/${3}" target="_blank"><img src="${1}://${2}.trillian.im/${3}" width="200"/></a>', $text);
 
-            if (isset($messages[$messageId])) {
-                fwrite(STDERR, sprintf("Skipping duplicate message: %s - %s\n", $date->format("r"), $text));
-                continue;
+            if ($firstDate === null) {
+                $firstDate = $date;
             }
 
-            $messages[$messageId] = array
+            $messages[] = array
             (
                 "date" => $date,
                 "user" => $user,
@@ -106,21 +159,23 @@ foreach ($perContactFiles as $contactName => $files) {
             );
         }
 
-        if (isset($files[$fileIndex + 1])) {
-            /**
-             * @var $nextFile SplFileInfo
-             */
-            $nextFile = $files[$fileIndex + 1];
+        if (empty($messages)) {
+            fwrite(STDERR, "No messages found, skipping\n");
+            continue;
+        }
 
-            $nextLog = str_replace("/", "-", $nextFile->getRelativePath()) . ".html";
+        if (isset($perDayMessages[$dayIndex + 1])) {
+            $nextDate = $perDayMessages[$dayIndex + 1][0][0];
+
+            $nextLog = getFilenameFromDate($nextDate);
         } else {
             $nextLog = null;
         }
 
-        $outputFilename = str_replace("/", "-", $file->getRelativePath());
+        $outputFilename = getFilenameFromDate($firstDate);
 
         try {
-            $html = $twig->render("template.twig", array
+            $html = $twig->render(sprintf("%s.twig", $template), array
             (
                 "ownName" => $ownName,
                 "contactName" => $contactName,
@@ -129,11 +184,11 @@ foreach ($perContactFiles as $contactName => $files) {
                 "nextLog" => $nextLog
             ));
 
-            $filesystem->dumpFile(sprintf("%s/%s/%s.html", $outputDir, $contactName, $outputFilename), $html);
+            $filesystem->dumpFile(sprintf("%s/%s/%s", $outputDir, $contactName, $outputFilename), $html);
         } catch (Exception $exception) {
             fwrite(STDERR, $exception->getTraceAsString());
         }
 
-        $previousLog = $outputFilename . ".html";
+        $previousLog = $outputFilename;
     }
 }
